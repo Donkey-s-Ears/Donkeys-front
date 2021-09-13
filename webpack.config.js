106780d38da2ba5caf05bcf4629a818dcab68b07
deathnet <53890887+deathnet@users.noreply.github.com>
2021-09-13 16:04:55 +0900

    environment/DON-10
    프론트엔드 개발 환경 구축

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const port = process.env.PORT || 3000;
+const mode = process.env.NODE_NEV || "development";
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_moudles/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./template/index.html",
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
+  mode: mode,
+  devServer: {
+    historyApiFallback: true,
+    host: "localhost",
+    port: port,
+    open: true,
+  },
+  performance: { hints: false },
+  optimization: {
+    splitChunks: {
+      name: "vender",
+      chunks: "initial",
+    },
+    minimize: true,
+    emitOnErrors: true,
+  },
+  devtool: "cheap-module-source-map",
+};
